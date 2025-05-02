7d45223085e5bd90c83799a26d9604b27cd28432
kahfi <kahfialfath@gmail.com>
2025-05-02 17:37:32 +0700

    first commit

---
+++ src/main/java/com/demo/traveloka/website/automation/DemoTravelokaAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.traveloka.website.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTravelokaAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTravelokaAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/traveloka/website/automation/DemoTravelokaAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.traveloka.website.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTravelokaAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
